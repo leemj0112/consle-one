b54c663c3465bb2be1b707eac8d71cf0ea434546
leemj0112 <163072105+leemj0112@users.noreply.github.com>
2024-03-12 11:18:08 +0900

    GiftBox 추가
    Letter, Money, 배송기사님도 넣어줬다
    Public, Instance도 넣었다

--- my program/Program.cs
+++ my program/Program.cs
@@ -1,10 +1,25 @@
 ﻿namespace my_program
 {
+    class GiftBox
+    {
+        public string Letter;
+        public int Money;
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GiftBox a = new GiftBox()
+            {
+                Letter = "잘 지내지?",
+                Money = 10000
+            };
+
+            GiftBox 배송기사님 = a;
+
+            a.Letter = "어떻게 지내고 있어?";
+            a.Money = 15000;
         }
     }
 }
