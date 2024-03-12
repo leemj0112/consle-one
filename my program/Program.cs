8fec7fd0631535a1845da9bd988e021c24bc6c1f
leemj0112 <163072105+leemj0112@users.noreply.github.com>
2024-03-12 09:53:25 +0900

    Initial commit

---
+++ my program/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace my_program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
